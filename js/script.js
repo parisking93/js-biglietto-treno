db9df531f36a92013309ea4064170d6b75355730
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-27 14:43:10 +0200

    add input utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// input utente 
+var eta = prompt('Quanti hanni hai ?');
+var km = prompt('Quanti chilometri devi percorrere ?');
+// fine input utente 
+
+console.log(eta);
+console.log(km);
